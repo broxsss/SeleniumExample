6a148e0a60db6904d56701e0af7adbf6974f512f
broxsss <broxsss@gmail.com>
2017-06-25 17:56:49 +0530

    robot upload class

--- com.EGIT/src/test/java/com/Git/com/EGIT/fun.java
+++ com.EGIT/src/test/java/com/Git/com/EGIT/fun.java
@@ -1,50 +1,55 @@
 package com.Git.com.EGIT;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Ordering;
+
 public class fun {
 	
-	public static boolean compare(String val1,String val2)
-	{
-		if(val1==null && val2==null)
-		{
-			return true;
-		}
-
-		else if(val1==null || val2==null){
-			return false;
-		}
-
-		else if(val1.equals(val2)){
-			return true;
-		  	
-		}
-    return false;
+	public static boolean isSorted(int[] a){
+	    boolean isSorted = true;
+	    boolean isAscending = a[1] > a[0];
+	    if(isAscending) {
+	        for (int i = 0; i < a.length-1; i++) {
+	            if(a[i] > a[i+1]) {
+	                isSorted = false;
+	                break;
+	            }           
+	        }
+	    } else {//descending
+	        for (int i = 0; i < a.length-1; i++) {
+	            if(a[i] < a[i+1]) {
+	                isSorted = false;
+	                break;
+	            }           
+	        }  
+	    }    
+	    return isSorted;
 	}
-	
-	public static boolean compare1(String val1,String val2)
+	public static void main(String[] args)
 	{
-		if(val1==null && val2==null)
-		{
-			return true;
-		}
-
-		else if(val1==null || val2==null){
-			return false;
-		}
-
-		else{
-			boolean rc = val1.equals(val2);
-			return true;
-		}
-
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String ab = "1123";
-        String bc = "1123";
-       System.out.println(compare(ab,bc)); 
-       System.out.println(compare1(ab,bc)); 
-       
+	          int ar[] = {5,6,300,1010};
+	          System.out.println(ar[0]+" "+ar[1]+" "+ ar[2]);
+	          isSorted(ar);
+	          System.out.println(isSorted(ar)); 
+	          List<Integer> list = new ArrayList<Integer>();
+	          list.add(2);
+	          list.add(6);
+	          list.add(8);
+	          list.add(10);
+	          list.add(12);
+	          list.add(100);
+	          list.add(122);
+	          list.add(130);
+	          list.add(null);
+	          System.out.println(list); 
+	        //  boolean sorted = Ordering.natural().isOrdered(list);
+	        //  System.out.println(sorted);
+	          boolean sorted1=   Ordering.natural().nullsLast().isOrdered(list);
+	          System.out.println(sorted1);
+	          boolean sorted2= Ordering.natural().reverse().isOrdered(list);
+	          System.out.println(sorted2);
 	}
 
 }
---
+++ com.EGIT/src/test/java/com/Git/com/EGIT/robot_upload.java
@@ -0,0 +1,108 @@
+package com.Git.com.EGIT;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class robot_upload {
+
+@Test
+public void upload() throws InterruptedException, AWTException
+{
+	System.setProperty("webdriver.chrome.driver","C:/akshay/chromedriver.exe");
+	Map<String,Object> prefs = new HashMap<String,Object>();
+	prefs.put("profile.default_content_setting_values.notifications", 2);
+	ChromeOptions option = new ChromeOptions();
+	option.addArguments("--disable-infobars");
+	option.addArguments("--disable-geolocation");
+	option.addArguments("test-type");
+	option.setExperimentalOption("prefs", prefs);
+	WebDriver driver = new ChromeDriver(option);
+	driver.manage().window().maximize();
+	StringSelection sel = new StringSelection("C:\\Users\\aksaini\\Desktop\\1.txt");
+	Toolkit.getDefaultToolkit().getSystemClipboard().setContents(sel,null);
+	System.out.println("selection" +sel);
+	// Open Monster.com
+	 driver.get("http://my.monsterindia.com/create_account.html");
+	 Thread.sleep(2000);
+	 String parent = driver.getWindowHandle();
+	 System.out.println("parent :  "+driver.getTitle());
+	 Set<String> st = driver.getWindowHandles();
+	 Iterator<String> it = st.iterator();
+	 while(it.hasNext())
+	 {
+		 String child = it.next();
+		 if(!parent.equals(child))
+		 {
+			 driver.switchTo().window(child);
+			 System.out.println("child  :"+driver.switchTo().window(child).getTitle());
+			 driver.close();
+			 
+		 }
+		 
+	 }
+	 driver.switchTo().window(parent);
+	 
+	 // Create object of Robot class
+	 Robot robot = new Robot();
+	 Thread.sleep(1000);
+	 
+	 
+	 robot.keyPress(KeyEvent.VK_DOWN);
+	 robot.keyPress(KeyEvent.VK_DOWN);
+	 robot.setAutoDelay(2000);
+	 robot.keyPress(KeyEvent.VK_DOWN);
+	 robot.keyRelease(KeyEvent.VK_DOWN);
+	 // This will scroll down the page 
+//	JavascriptExecutor js = (JavascriptExecutor)driver;
+//	 js.executeScript("scroll(0,350)");
+	 
+	// Wait for 5 seconds
+	 Thread.sleep(5000);
+	 
+	// This will click on Browse button
+	 driver.findElement(By.id("wordresume")).click();
+	 
+	 
+	 
+	 System.out.println("Browse button clicked");
+	 
+	
+	      
+	  // Press Enter
+
+	 robot.keyPress(KeyEvent.VK_ENTER);
+	 
+	// Release Enter
+	 robot.keyRelease(KeyEvent.VK_ENTER);
+	 
+	  // Press CTRL+V
+	 robot.keyPress(KeyEvent.VK_CONTROL);
+	 robot.keyPress(KeyEvent.VK_V);
+	 
+	// Release CTRL+V
+	 robot.keyRelease(KeyEvent.VK_CONTROL);
+	 robot.keyRelease(KeyEvent.VK_V);
+	 Thread.sleep(1000);
+	        
+	       //  Press Enter 
+	 robot.keyPress(KeyEvent.VK_ENTER);
+	 robot.keyRelease(KeyEvent.VK_ENTER);
+	
+	
+}
+
+}
