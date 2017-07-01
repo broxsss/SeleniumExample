d30ac1f51384b6973fea304c01f2b9b4bc8b0d2d
broxsss <broxsss@gmail.com>
2017-07-02 01:36:48 +0530

    commit now

---
+++ com.EGIT/src/test/java/com/Git/com/EGIT/imagereader.java
@@ -0,0 +1,48 @@
+package com.Git.com.EGIT;
+
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class imagereader {
+
+	public static void main(String args[])
+	{
+	
+		Image image = null;
+		  try {
+		      // Read from a file
+		      File sourceimage = new File("C:\\Users\\aksaini\\Desktop\\source.gif");
+		      image = ImageIO.read(sourceimage);
+		   
+		     /* // Read from an input stream
+		      InputStream is = new BufferedInputStream(
+		          new FileInputStream("C:\\Users\\aksaini\\Desktop\\source.gif"));
+		      image = ImageIO.read(is);
+		   
+		      // Read from a URL
+		      URL url = new URL("http://java-tips.org/source.gif");
+		      image = ImageIO.read(url);*/
+		  } catch (IOException e) {
+		  }
+		   
+		  // Use a label to display the image
+		  JFrame frame = new JFrame();
+		  JLabel label = new JLabel(new ImageIcon(image));
+		  frame.getContentPane().add(label, BorderLayout.CENTER);
+		  frame.pack();
+		  frame.setVisible(true);
+	
+	
+	}
+	
+	
+	
+}
+
+
---
+++ com.EGIT/src/test/java/com/Git/com/EGIT/multiplewindow.java
@@ -0,0 +1,51 @@
+package com.Git.com.EGIT;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class multiplewindow {
+	
+	@Test
+	public void multiplewindows()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:/akshay/chromedriver.exe");
+		Map<String,Object> prefs = new HashMap<String,Object>();
+	    prefs.put("profile.default_content_setting_values.notifications", 2);
+	    ChromeOptions option = new  ChromeOptions();
+		option.addArguments("test-type");
+		option.addArguments("--disable-infobars");
+		option.addArguments("--disable-geolocation");
+		option.setExperimentalOption("prefs", prefs);
+		WebDriver  driver = new ChromeDriver(option); 
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://www.naukri.com");
+		String parent = driver.getWindowHandle();
+		System.out.println(driver.getTitle());
+		Set<String> s1 =driver.getWindowHandles(); 
+		Iterator<String> itr = s1.iterator();
+		while(itr.hasNext())
+		{
+			String child = itr.next();
+			if(!parent.equals(child))
+			{
+				driver.switchTo().window(child);
+				String childs=driver.switchTo().window(child).getTitle();
+				System.out.println("child  :"+childs);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parent);
+		
+		
+	}
+
+}
